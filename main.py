70e4e3f2a907fb97d71528aa093c2ffe1530bf12
laplus <zww1837863015@gmail.com>
2021-03-07 17:17:47 +0800

    main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask('zerowide')
 
 @app.route('/')
-def helloWorld():
-    return 'hello,world'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run('127.0.0.1')
